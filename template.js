cf58c029be9d3187976fd4e0b16c9b741c66a819
jordanrogus <jordanrogus@gmail.com>
2023-12-01 09:31:21 -0700

    Initial commit

---
+++ template.js
@@ -0,0 +1,47 @@
+// To Run Code
+// Requires node
+// Open terminal inside folder and run 'node <output filename.js>'
+
+// Read input file and push it to an array named inputArray which is mapped appropriately per the problem statement
+const useExample = true;
+const fs = require("fs");
+const inputArray = [];
+if (useExample){
+    inputArray = fs
+    .readFileSync("./input-example").toString('utf-8')
+    .split("\n") // Split on new lines
+    .filter(Boolean); // Remove empty lines
+} else {
+    inputArray = fs
+    .readFileSync("./input").toString('utf-8')
+    .split("\n") // Split on new lines
+    .filter(Boolean); // Remove empty lines
+}
+
+// Initial log separator
+console.log("----------------------------------------------------------");
+
+// Log the raw input
+console.log("Input array:");
+console.log(inputArray);
+console.log("----------------------------------------------------------");
+
+// Part A
+function partA(inputArray){
+    return 'TBD';
+}
+
+// Log Part A
+console.log("Part a: \n" + partA(inputArray));
+console.log("----------------------------------------------------------");
+
+// Part B
+function partB(inputArray){
+    return 'TBD';
+}
+
+// Log Part B
+console.log("Part b: \n" + partB(inputArray));
+console.log("----------------------------------------------------------");
+
+//////////////////////// HELPER FUNCTION(S) ////////////////////////
